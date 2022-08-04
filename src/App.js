7d1e7bbdf55b170c008753ac7a7557fedbdcf6ba
이동형 <leedhhhhh@gmail.com>
2022-08-04 20:42:45 +0900

    markdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return(
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
     const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year`
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
     );
     const json = await response.json();
       setMovies(json.data.movies);
@@ -19,6 +23,31 @@ function Home() {
   return (
     <div>
       {loading ? <h1>Loading...</h1> :
+      // 메인화면
+      <Fragment>
+        <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         <div>
           {movies.map((movie) => 
             <Movie 
@@ -31,6 +60,7 @@ function Home() {
             />
           )}
         </div>
+      </Fragment>
       }
     </div>
   );
