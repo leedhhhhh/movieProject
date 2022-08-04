cbd3e1ae9aa38ebf3815038a88b375ddab25700f
이동형 <leedhhhhh@gmail.com>
2022-08-04 15:46:02 +0900

    basename add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/movie/:id" element={<Detail/>} />
